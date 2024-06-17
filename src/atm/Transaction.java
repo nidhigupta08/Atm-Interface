d84e8a4567cd27acd8473f3fc3988eee8ab04446
Nidhi <guptanidhi8000@gmail.com>
2024-06-17 22:56:06 +0530

    Add Transaction class to record transaction details

---
+++ src/atm/Transaction.java
@@ -0,0 +1,20 @@
+package atm;
+
+import java.util.Date;
+
+public class Transaction {
+    private String type;
+    private double amount;
+    private Date date;
+
+    public Transaction(String type, double amount) {
+        this.type = type;
+        this.amount = amount;
+        this.date = new Date();
+    }
+
+    @Override
+    public String toString() {
+        return date + " - " + type + ": " + amount;
+    }
+}
